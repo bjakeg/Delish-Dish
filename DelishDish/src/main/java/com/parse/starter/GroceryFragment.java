80a2ef786049a3be075f20501c3db1b3e2fd4d98
bjakeg <bjakeg@gmail.com>
2016-03-15 13:48:57 -0500

    Added basic files for Grocery List

---
+++ DelishDish/src/main/java/com/parse/starter/GroceryFragment.java
@@ -0,0 +1,30 @@
+package com.parse.starter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by jakegraham on 3/15/16.
+ */
+public class GroceryFragment extends Fragment {
+    ListView listView;
+
+    public GroceryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_cookbook, container, false);
+
+        listView = (ListView) rootView.findViewById(R.id.grocery_list_view);
+
+        return rootView;
+    }
+}
